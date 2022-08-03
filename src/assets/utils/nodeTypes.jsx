5992728bf770445b7917eb7b617a66422a883adb
Жиленко Евгений <e.zhilenko@worksolutions.ru>
2022-08-03 10:16:45 +0300

    Добавил типы нод

---
+++ src/assets/utils/nodeTypes.jsx
@@ -0,0 +1,10 @@
+import CustomNode from "../../components/customNode";
+
+export const nodeTypes = {
+  ellipse: () => <CustomNode nodeName={'ellipse'}/>,
+  rectangle: () => <CustomNode nodeName={'rectangle'}/>,
+  rhombus: () => <CustomNode nodeName={'rhombus'}/>,
+  rectSections: () => <CustomNode nodeName={'rectSections'}/>,
+  parallelogram: () => <CustomNode nodeName={'parallelogram'} handlesStyle={ {left: {left: 10}, right: {right: 10} }}/>,
+  sixCorn: () => <CustomNode nodeName={'sixCorn'}/>,
+};
--- src/components/Flow.jsx
+++ src/components/Flow.jsx
@@ -1,60 +1,38 @@
-import React, { useRef } from 'react';
-import ReactFlow, {MiniMap, Controls, Background, useEdgesState, useNodesState} from 'react-flow-renderer/nocss';
+import React, {useCallback, useRef} from 'react';
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useEdgesState,
+  useNodesState,
+  addEdge
+} from 'react-flow-renderer/nocss';
 import 'react-flow-renderer/dist/style.css'
 import 'react-flow-renderer/dist/theme-default.css'
-import EllipseNode from "./customNodes/Ellipse";
-
-const nodeTypes = { ellipse: EllipseNode };
-
-// const initialNodes = [
-//   {
-//     id: '1',
-//     type: 'input',
-//     data: { label: 'Input Node' },
-//     position: { x: 250, y: 25 },
-//   },
-//
-//   // {
-//   //   id: '2',
-//   //   // you can also pass a React component as a label
-//   //   data: { label: <div>Default Node</div> },
-//   //   position: { x: 100, y: 125 },
-//   // },
-//   {
-//     id: '3',
-//     type: 'output',
-//     data: { label: 'Output Node' },
-//     position: { x: 250, y: 250 },
-//   },
-//   {
-//     id: '4',
-//     type: 'ellipse',
-//     data: { label: 'Output Node' },
-//     position: { x: 300, y: 250 },
-//   },
-// ];
-
-const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2' },
-  { id: 'e2-3', source: '2', target: '3', animated: true },
-];
+import { nodeTypes } from "../assets/utils/nodeTypes";
 
 function Flow() {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   const id = useRef(1);
+  const onConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
   return (
     <ReactFlow
       onDragOver={(event) => {event.preventDefault()}}
       onDrop={(event) => {
         event.preventDefault();
+        const nodeType = event.dataTransfer.getData('text')
         const nodesCopy = [...nodes];
         const newNode = {
           id: `${id.current++}`,
-          type: 'ellipse',
-          data: { label: 'Output Node' },
+          type: `${nodeType}`,
+          data: { label: 'Main' },
           position: { x: 250, y: 250 },
         }
+
         nodesCopy.push(newNode);
         setNodes(nodesCopy);
       }}
@@ -62,6 +40,8 @@ function Flow() {
       nodes={nodes}
       edges={edges}
       onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
       fitView
     >
       <MiniMap />
