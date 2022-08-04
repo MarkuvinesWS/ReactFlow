eb7b4fc2b858e57e2de77ba5dbefa9ebc232b92f
Жиленко Евгений <e.zhilenko@worksolutions.ru>
2022-08-04 10:54:06 +0300

    Вынес обработчик в отдельную функцию

--- src/components/Flow.jsx
+++ src/components/Flow.jsx
@@ -1,46 +1,47 @@
-import React, {useCallback, useRef, useState} from 'react';
+import React, {useCallback} from 'react';
 import ReactFlow, {
   MiniMap,
   Controls,
   Background,
   useEdgesState,
   useNodesState,
-  addEdge
+  addEdge, useReactFlow
 } from 'react-flow-renderer/nocss';
 import 'react-flow-renderer/dist/style.css'
 import 'react-flow-renderer/dist/theme-default.css'
 import { nodeTypes } from "../assets/utils/nodeTypes";
+import {edgeTypes} from "../assets/utils/edgeTypes";
 
-function Flow({getFlowInstance}) {
+function Flow() {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const [flowInst, setFlowInst] = useState(null);
-
-  const id = useRef(1);
+  const { project } = useReactFlow();
   const onConnect = useCallback(
     (connection) => setEdges((eds) => addEdge(connection, eds)),
     [setEdges]
   );
+
+  function onDropHandler(event) {
+    event.preventDefault();
+    const nodeType = event.dataTransfer.getData('text')
+    const nodesCopy = [...nodes];
+    const newNode = {
+      id: `${Math.random() * 10}`,
+      type: `${nodeType}`,
+      data: { innerText: '' },
+      position: project({x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY}),
+    }
+    nodesCopy.push(newNode);
+    setNodes(nodesCopy);
+  }
   return (
     <ReactFlow
-      onInit={(inst) => {setFlowInst(inst); getFlowInstance(inst)}}
       onDragOver={(event) => {event.preventDefault()}}
-      onDrop={(event) => {
-        event.preventDefault();
-        const nodeType = event.dataTransfer.getData('text')
-        const nodesCopy = [...nodes];
-        const newNode = {
-          id: `${id.current++}`,
-          type: `${nodeType}`,
-          data: { label: 'Main' },
-          position: flowInst.project({x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY}),
-        }
-        nodesCopy.push(newNode);
-        setNodes(nodesCopy);
-      }}
+      onDrop={onDropHandler}
       nodeTypes={nodeTypes}
       nodes={nodes}
       edges={edges}
+      edgeTypes={edgeTypes}
       onNodesChange={onNodesChange}
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
