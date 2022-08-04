8bc681ddc33ce2606eacedc0db9d9f6cdc67606c
Жиленко Евгений <e.zhilenko@worksolutions.ru>
2022-08-04 10:50:00 +0300

    Прокинул все данные в пропсы

--- src/assets/utils/nodeTypes.jsx
+++ src/assets/utils/nodeTypes.jsx
@@ -1,13 +1,13 @@
 import CustomNode from "../../components/customNode";
 
 export const nodeTypes = {
-  ellipse: () => <CustomNode nodeName={'ellipse'}/>,
-  rectangle: () => <CustomNode nodeName={'rectangle'}/>,
-  rhombus: () => <CustomNode nodeName={'rhombus'}/>,
-  rectSections: () => <CustomNode nodeName={'rectSections'}/>,
-  parallelogram: () => <CustomNode nodeName={'parallelogram'} handlesStyle={ {left: {left: 10}, right: {right: 10} }}/>,
-  sixCorn: () => <CustomNode nodeName={'sixCorn'}/>,
-  rectUp: () => <CustomNode nodeName={'rectUp'}/>,
-  rectDown: () => <CustomNode nodeName={'rectDown'}/>,
-  circle: () => <CustomNode nodeName={'circle'} handlesStyle={{left: {left: 37}, right: {right: 37} }}/>,
+  ellipse: (data) => <CustomNode {...data}/>,
+  rectangle: (data) => <CustomNode {...data}/>,
+  rhombus: (data) => <CustomNode {...data}/>,
+  rectSections: (data) => <CustomNode {...data}/>,
+  parallelogram: (data) => <CustomNode {...data} handlesStyle={ {left: {left: 10}, right: {right: 10} }}/>,
+  sixCorn: (data) => <CustomNode {...data}/>,
+  rectUp: (data) => <CustomNode {...data}/>,
+  rectDown: (data) => <CustomNode {...data}/>,
+  circle: (data) => <CustomNode {...data} handlesStyle={{left: {left: 37}, right: {right: 37} }}/>,
 };
