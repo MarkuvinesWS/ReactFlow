9e29e636554ead09edc6f100813bfdbb3f5cc57c
Жиленко Евгений <e.zhilenko@worksolutions.ru>
2022-08-04 10:21:28 +0300

    Добавил ReactFlowProvider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import MainPanel from "./components/MainPanel";
 import './assets/styles/nodes.css'
+import {ReactFlowProvider} from "react-flow-renderer/nocss";
 
 function App() {
   return (
     <div className="App">
+      <ReactFlowProvider>
       <MainPanel/>
+      </ReactFlowProvider>
     </div>
   );
 }
